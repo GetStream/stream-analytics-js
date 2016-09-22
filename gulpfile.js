5a8d2a2d68a10a475bbbf53600b327c6841bb1b3
Matthisk Heimensen <m@tthisk.nl>
2016-09-22 16:03:43 +0200

    Disable jscs

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ function runSynchronized(tasks, callback){
 gulp.task('lint', function() {
   return gulp.src('./src/**/*.js')
     .pipe(jshint({ lookup: true }))
-    .pipe(jscs())
+    // .pipe(jscs())
     .pipe(stylish.combineWithHintResults())
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(jshint.reporter('fail'));
