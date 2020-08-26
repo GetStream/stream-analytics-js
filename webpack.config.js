013ce8ad65e2c468654ad535d3431c8d0403ca24
Amin Mahboubi <aminmahboobi@gmail.com>
2020-08-26 15:25:24 +0200

    fix builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 const path = require('path');
 
-module.exports = {
-    entry: ['./src/stream-analytics.js'],
+module.exports = (env, argv = []) => ({
+    entry: ['./src/stream-analytics.ts'],
 
     mode: 'production',
+
+    module: {
+        rules: [{ test: /\.ts?$/, use: 'ts-loader', exclude: /node_modules/ }],
+    },
+
+    resolve: { extensions: ['.ts', '.js'] },
+
+    optimization: {
+        minimize: argv.minify !== undefined,
+    },
 
     output: {
         path: path.join(__dirname, 'dist', 'js'),
         publicPath: 'dist/',
-        filename: 'stream-analytics.js',
+        filename: argv.minify !== undefined ? 'stream-analytics.min.js' : 'stream-analytics.js',
         chunkFilename: '[chunkhash].js',
         library: 'StreamAnalytics',
         libraryTarget: 'umd',
     },
-};
+});
