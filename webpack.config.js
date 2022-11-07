9dd82faaf8d54c06f878df9aa668f54c09ec9b81
ferhat elmas <elmas.ferhat@gmail.com>
2022-11-07 18:06:56 +0100

    fix: security upgrades (#59)

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
-module.exports = (env, argv = []) => ({
+module.exports = (env = {}) => ({
     entry: ['./src/stream-analytics.ts'],
 
-    mode: 'production',
+    mode: env.production ? 'production' : 'development',
 
     module: {
         rules: [{ test: /\.ts?$/, use: 'ts-loader', exclude: /node_modules/ }],
@@ -11,14 +11,10 @@ module.exports = (env, argv = []) => ({
 
     resolve: { extensions: ['.ts', '.js'] },
 
-    optimization: {
-        minimize: argv.minify !== undefined,
-    },
-
     output: {
         path: path.join(__dirname, 'dist', 'js'),
         publicPath: 'dist/',
-        filename: argv.minify !== undefined ? 'stream-analytics.min.js' : 'stream-analytics.js',
+        filename: env.production ? 'stream-analytics.min.js' : 'stream-analytics.js',
         chunkFilename: '[chunkhash].js',
         library: 'StreamAnalytics',
         libraryTarget: 'umd',
